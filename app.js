3411b8bdfffedfddc6eb9efd350282e39aa3edce
Maxim <max.degtyareff@yandex.ru>
2016-09-15 21:09:25 -0700

    15 september

--- app.js
+++ app.js
@@ -2,23 +2,23 @@ var express = require('express'),
     app = express(),
     co = require('co'),
     logger = require('./lib/logs'),
-    path = require('path');
+    path = require('path'),
+    indexRouter = require('./routes/index');
 
-//Sets
+//Движки и кеширование
+
 app.set('views', path.join(__dirname,'/pages'));
 app.set('view engine', 'pug');
 app.set('view cache');
 
-//Middleware
+//Middlewares
 app.use(express.static(path.join(__dirname, '/static')));
+app.use('/', indexRouter);
 
-app.get('/',function(req,res){
-    res.render('p/index');
-})
+
+
 //Starting server
-
 var port = process.env.PORT || 3000;
-
 app.listen(port,function(){
     console.log('Great! Server has started on port: ' + port);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,20 @@
 var express = require('express');
-var router = express.Router;
+var router = express.Router();
 
 router.get('/', function(req,res){
-    
-})
+    res.render('p/index');
+});
+
+router.get('/author',function(req,res){
+    var authorname = req.query.name;
+    if (!authorname) authorname='Maxim Degtyarev';
+
+    res.render('p/about', {
+        name: authorname,
+        originame: true
+    });
+
+});
+
+
+module.exports = router;
